0ed8d21dbf36276b5896fa4d84817bc06f7e5ce4
mayone-du <cocomayo1201@icloud.com>
2021-04-02 13:01:38 +0900

    Update getIdea-test

--- src/lib/getIdeas.tsx
+++ src/lib/getIdeas.tsx
@@ -1,3 +1,8 @@
-// const getIdeas = () => {
-//   fetch()
-// }
+const getIdeas = async () => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_RESTAPI_URL}api/ideas-list/`);
+  const ideas = await res.json();
+  await console.log(ideas)
+  return ideas;
+}
+
+export { getIdeas };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,48 @@
-import { GetStaticProps } from "next";
+import { getIdeas } from "../lib/getIdeas";
+// import { GetStaticProps } from "next";
 
-const Home: React.FC = () => {
+type Ideas = [
+  {
+    id: number;
+    title: string;
+    contents: string;
+    is_published: boolean;
+    created_at: string;
+    updated_at: string;
+  }
+];
+
+const Home: React.FC<{ ideas: Ideas }> = ({ ideas }) => {
   return (
     <>
       <h1>Hello, World!!</h1>
+      <div>
+        {ideas.map((idea) => {
+          return (
+            <div className='m-20'>
+              <div>id - {idea.id}</div>
+              <div>title - {idea.title}</div>
+              <div>contents - {idea.contents}</div>
+              <div>is_published - {idea.is_published}</div>
+              <div>created_at - {idea.created_at}</div>
+              <div>updated_at - {idea.updated_at}</div>
+            </div>
+          );
+        })}
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default Home;
 
+const getStaticProps = async () => {
+  const ideas = await getIdeas();
+  return {
+    props: {
+      ideas,
+    },
+  };
+};
 
-
-// const getStaticProps: GetStaticProps = () => {
-  
-// }
-
-// export { getStaticProps };
+export { getStaticProps };
