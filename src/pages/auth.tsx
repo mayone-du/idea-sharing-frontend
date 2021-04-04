8090857914b3d3203b6b5913476c36c405ef3477
mayone-du <cocomayo1201@icloud.com>
2021-04-04 09:00:54 +0900

    Create login,register func

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,60 +1,99 @@
 import { TextField, Button } from "@material-ui/core";
 import { useState } from "react";
-import Cookie from 'universal-cookie';
+import Cookie from "universal-cookie";
+import { useRouter } from 'next/router';
 
 const cookie = new Cookie();
 
 const Auth: React.VFC = () => {
 
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+
+  const router = useRouter();
+
+  const [isLogin, setIsLogin] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   const usernameChange = (e: any) => {
     setUsername(e.target.value);
-  }
+  };
 
   const passwordChange = (e: any) => {
     setPassword(e.target.value);
-  }
+  };
 
   const login = async () => {
-    await fetch(`${process.env.NEXT_PUBLIC_RESTAPI_URL}api/auth/jwt/create/`,
-    {
-      method: 'POST',
+    await fetch(`${process.env.NEXT_PUBLIC_RESTAPI_URL}api/auth/jwt/create/`, {
+      method: "POST",
       body: JSON.stringify({ username: username, password: password }),
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
-    }).then((res) => {
-      if (res.status === 401) {
-        throw new Error('non_fields_error');
-      } else if (res.ok) {
-        return res.json();
-      }
-    }).then((data) => {
-      const accessOptions = { path: '/' };
-      const refreshOptions = { path: '/', maxAge: 60 * 60 * 24 * 7};
-      // const refreshOptions = { path: '/', expires: new Date(Date.now() + 1) };
-      cookie.set('access_token', data.access, accessOptions );
-      cookie.set('refresh_token', data.refresh, refreshOptions );
-      console.log(cookie.getAll())
     })
-  }
+      .then((res) => {
+        if (res.status === 401) {
+          throw new Error("non_fields_error");
+        } else if (res.ok) {
+          return res.json();
+        }
+      })
+      .then((data) => {
+        const accessOptions = { path: "/", maxAge: 60 * 60 };
+        const refreshOptions = { path: "/", maxAge: 60 * 60 * 24 * 7 };
+        // const refreshOptions = { path: '/', expires: new Date(Date.now() + 1) };
+        cookie.set("access_token", data.access, accessOptions);
+        cookie.set("refresh_token", data.refresh, refreshOptions);
+        console.log(cookie.getAll());
+        router.push('/');
+      });
+  };
+
+  const register = async () => {
+    await fetch(`${process.env.NEXT_PUBLIC_RESTAPI_URL}api/auth/register/`, {
+      method: "POST",
+      body: JSON.stringify({ username: username, password: password }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    login();
+  };
 
   return (
     <>
       <div>
-        <div className='m-10'>
+        <h1 className="text-4xl text-center my-4">ログイン・新規登録画面</h1>
+        <Button
+          variant="outlined"
+          onClick={() => {
+            setIsLogin(!isLogin);
+          }}
+        >
+          changeMode
+        </Button>
+        <div className="m-10">
           <p>username</p>
-          <TextField type='text' variant="outlined" onChange={usernameChange} />
+          <TextField
+            type="text"
+            placeholder="username"
+            variant="outlined"
+            onChange={usernameChange}
+          />
         </div>
-        <div className='m-10'>
+        <div className="m-10">
           <p>password</p>
-          <TextField type='password' variant="outlined" onChange={passwordChange} />
+          <TextField
+            type="password"
+            placeholder="password"
+            variant="outlined"
+            onChange={passwordChange}
+          />
         </div>
 
         <div>
-          <Button onClick={login} variant='contained'>button</Button>
+          <Button onClick={isLogin ? login : register} variant="contained">
+            {isLogin ? 'login' : 'register' }
+          </Button>
         </div>
       </div>
     </>
