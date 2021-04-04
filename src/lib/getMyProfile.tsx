99e1947ebe66c78925eaf7959a1be0b67f829ebc
mayone-du <cocomayo1201@icloud.com>
2021-04-04 22:12:36 +0900

    Update getMyProfile

--- src/lib/getMyProfile.tsx
+++
@@ -1,7 +0,0 @@
-const getMyProfile = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_RESTAPI_URL}api/my-profile/`);
-  const profile = await res.json();
-  return profile;
-}
-
-export { getMyProfile };
--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,5 +1,10 @@
 import { getUsers } from "../lib/getUsers";
+import Cookie from "universal-cookie";
+
+const cookie = new Cookie();
+
 import { Layout } from "../components/Layout";
+import { useEffect, useState } from "react";
 
 type Users = [
   {
@@ -9,11 +14,52 @@ type Users = [
   }
 ];
 
+
 const Profile: React.VFC<{ users: Users }> = ({ users }) => {
+
+  const [loginUser, setLoginUser] = useState([{ id: 0, username: '', profile_text: ''}]);
+
+  const getMyProfile = async () => {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_RESTAPI_URL}api/my-profile/`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `JWT ${cookie.get("access_token")}`,
+        },
+      }
+    );
+    // console.log(res);
+    const profile = await res.json();
+    setLoginUser(profile);
+    // console.log(profile);
+    return profile;
+  };
+
+
+  useEffect(() => {
+    getMyProfile();
+  }, [])
+
   return (
     <>
         <Layout metaTitle="profile">
           <h1 className="text-4xl text-center my-4">profile</h1>
+
+
+          <div className="m-40">
+            <h2 className='text-3xl text-center mt-20'>my profile</h2>
+            {loginUser.map((item, index) => {
+              return (
+                <div key={index.toString()}>
+                  <p>{item.id}</p>
+                  <p>{item.username}</p>
+                  <p>{item.profile_text}</p>
+                </div>
+              )
+            })}
+          </div>
 
           <div>
             <h2 className='text-3xl text-center mt-20'>users-list</h2>
