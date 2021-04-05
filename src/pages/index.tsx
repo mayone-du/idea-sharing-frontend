34ddd8bdba2084cc8ed832e2892b497d5d2d2e08
mayone-du <cocomayo1201@icloud.com>
2021-04-05 23:27:22 +0900

    Update index ideas sort

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
-import { Layout } from '../components/Layout';
+import { Layout } from "../components/Layout";
 import { getIdeas } from "../lib/getIdeas";
 import { getComments } from "../lib/getComments";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 // import { GetStaticProps } from "next";
 
 type Ideas = [
@@ -26,16 +26,14 @@ type Comments = [
   }
 ];
 
-const Home: React.FC<{ ideas: Ideas; comments: Comments }> = ({
-  ideas,
+const Home: React.FC<{ sortedIdeas: Ideas; comments: Comments }> = ({
+  sortedIdeas,
   comments,
 }) => {
-
-
   // const [isLogin, setIsLogin] = useState(false);
   // const [username, setUsername] = useState('')
   // const [password, setPassword] = useState('')
-  
+
   // const jwtCreate = async () => {
   //   await fetch(`${process.env.NEXT_PUBLIC_RESTAPI_URL}api/auth/jwt/create/`,
   //   {
@@ -51,52 +49,44 @@ const Home: React.FC<{ ideas: Ideas; comments: Comments }> = ({
   //   })
   // }
 
-
-
-
-
-
-
-
+  useEffect(() => {
+    console.log(sortedIdeas);
+  }, []);
 
   return (
     <>
-    <Layout
-      metaTitle='HOME'
-    >
-      <h1>Hello, World!!</h1>
-      <div>
-        {ideas.map((idea) => {
-          return (
-            <div key={idea.id.toString()} className="m-20">
-              <div>id - {idea.id}</div>
-              <div>title - {idea.title}</div>
-              <div>contents - {idea.contents}</div>
-              <div>is_published - {idea.is_published.toString()}</div>
-              <div>createuser - {idea.createuser.toString()}</div>
-              <div>created_at - {idea.created_at}</div>
-              <div>updated_at - {idea.updated_at}</div>
-            </div>
-          );
-        })}
-      </div>
+      <Layout metaTitle="HOME">
+        <h1>Hello, World!!</h1>
+        <div>
+          {sortedIdeas.map((idea) => {
+            return (
+              <div key={idea.id.toString()} className="m-20">
+                <div>id - {idea.id}</div>
+                <div>title - {idea.title}</div>
+                <div>contents - {idea.contents}</div>
+                <div>is_published - {idea.is_published.toString()}</div>
+                <div>createuser - {idea.createuser.toString()}</div>
+                <div>created_at - {idea.created_at}</div>
+                <div>updated_at - {idea.updated_at}</div>
+              </div>
+            );
+          })}
+        </div>
 
-      <div>
-        {comments.map((comment) => {
-          return (
-          <div key={comment.id.toString()} className="m-10">
-            <div>comment id - {comment.id.toString()}</div>
-            <div>idea id - {comment.idea}</div>
-            <div>comment_user id - {comment.comment_user.toString()}</div>
-            <div>comment - {comment.comment}</div>
-            <div>created_at - {comment.created_at}</div>
-          </div>
-          )
-        })}
-      </div>
-
-    </Layout>
-
+        <div>
+          {comments.map((comment) => {
+            return (
+              <div key={comment.id.toString()} className="m-10">
+                <div>comment id - {comment.id.toString()}</div>
+                <div>idea id - {comment.idea}</div>
+                <div>comment_user id - {comment.comment_user.toString()}</div>
+                <div>comment - {comment.comment}</div>
+                <div>created_at - {comment.created_at}</div>
+              </div>
+            );
+          })}
+        </div>
+      </Layout>
     </>
   );
 };
@@ -104,11 +94,14 @@ const Home: React.FC<{ ideas: Ideas; comments: Comments }> = ({
 export default Home;
 
 const getStaticProps = async () => {
-  const ideas = await getIdeas();
+  const ideas: Ideas = await getIdeas();
+  const sortedIdeas: Ideas = ideas.sort((a, b) => {
+    return a < b ? 1 : -1;
+  });
   const comments = await getComments();
   return {
     props: {
-      ideas,
+      sortedIdeas,
       comments,
     },
     revalidate: 10,
